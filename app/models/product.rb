2eeef8f0199e0988372a74f41e892c66862eb17d
Ueverton-oliveira <ueverton.souz@gmail.com>
2020-11-19 15:46:00 -0400

    Renderização de Erro

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :productable, polymorphic: true
-  has_many :product_categories, dependent: :destroy
-  has_many :categories, through: :product_categories
-
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
+
+  has_many :product_categories, dependent: :destroy
+  has_many :categories, through: :product_categories
+
+  has_one_attached :image
+  validates :image, presence: true
 end
