ad942d27fa4961606f12c8cc70326793cb711a84
Ueverton-oliveira <ueverton.souz@gmail.com>
2020-11-19 16:36:22 -0400

    delet

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -1,5 +1,7 @@
 module Admin::V1
-  class CategoriesController < ApplicationController
+  class CategoriesController < ApiController
+    before_action :load_category, only: [:update, :destroy]
+    
     def index
       @categories = Category.all
     end
@@ -9,9 +11,23 @@ module Admin::V1
       @category.attributes = category_params
       save_category!
     end
+    
+    def update
+      @category.attributes = category_params
+      save_category!
+    end
 
-      
+    def destroy
+      @category.destroy!
+    rescue
+      render_error(fields: @category.errors.messages)
+    end
+
     private
+
+    def load_category
+      @category = Category.find(params[:id])
+    end
 
     def category_params
       return {} unless params.has_key?(:category)
--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -66,4 +66,90 @@ RSpec.describe "Admin::V1::Categories", type: :request do
       end
     end
   end
+
+  context "PATCH /categories/:id" do
+    let(:category) { create(:category) }
+    let(:url) { "/admin/v1/categories/#{category.id}" }
+    
+    context "with valid params" do
+      let(:new_name) { 'My new Category' }
+      let(:category_params) { { category: { name: new_name } }.to_json }
+      
+      it 'updates Category' do
+        patch url, headers: auth_header(user), params: category_params
+        category.reload
+        expect(category.name).to eq new_name
+      end
+
+      it 'returns updated Category' do
+        patch url, headers: auth_header(user), params: category_params
+        category.reload
+        expected_category = category.as_json(only: %i(id name))
+        expect(body_json['category']).to eq expected_category
+      end
+      
+      it 'returns success status' do
+        patch url, headers: auth_header(user), params: category_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  
+    context "with invalid params" do
+      let(:category_invalid_params) do 
+        { category: attributes_for(:category, name: nil) }.to_json
+      end
+      it 'does not update Category' do
+        old_name = category.name
+        patch url, headers: auth_header(user), params: category_invalid_params
+        category.reload
+        expect(category.name).to eq old_name
+      end
+
+      it 'returns error message' do
+        patch url, headers: auth_header(user), params: category_invalid_params
+        expect(body_json['errors']['fields']).to have_key('name')
+      end
+
+      it 'returns unprocessable_entity status' do
+        patch url, headers: auth_header(user), params: category_invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  context "DELETE /categories/:id" do
+    let!(:category) { create(:category) }
+    let(:url) { "/admin/v1/categories/#{category.id}" }
+  
+    it 'removes Category' do
+      expect do  
+        delete url, headers: auth_header(user)
+      end.to change(Category, :count).by(-1)
+    end
+  
+    it 'returns success status' do
+      delete url, headers: auth_header(user)
+      expect(response).to have_http_status(:no_content)
+    end
+
+    it 'does not return any body content' do
+      delete url, headers: auth_header(user)
+      expect(body_json).to_not be_present
+    end
+
+    it 'removes all associated product categories' do
+      product_categories = create_list(:product_category, 3, category: category)
+      delete url, headers: auth_header(user)
+      expected_product_categories = ProductCategory.where(id: product_categories.map(&:id))
+      expect(expected_product_categories.count).to eq 0
+    end
+
+    it 'does not remove unassociated product categories' do
+      product_categories = create_list(:product_category, 3)
+      delete url, headers: auth_header(user)
+      present_product_categories_ids = product_categories.map(&:id)
+      expected_product_categories = ProductCategory.where(id: present_product_categories_ids)
+      expect(expected_product_categories.ids).to contain_exactly(*present_product_categories_ids)
+    end
+  end
 end
