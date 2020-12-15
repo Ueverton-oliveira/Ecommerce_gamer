458860c4d5d0b792f3e743b2f82b45a4cd86ac81
Ueverton-oliveira <ueverton.souz@gmail.com>
2020-12-15 10:54:48 -0400

    Gerando controllers e rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
+
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth/v1/user'
-
-  namespace :admin do
+  
+  namespace :admin, defaults: { format: :json } do
     namespace :v1 do
       get "home" => "home#index"
       resources :categories
+      resources :coupons
       resources :products
+      resources :system_requirements
+      resources :users
     end
   end
 
